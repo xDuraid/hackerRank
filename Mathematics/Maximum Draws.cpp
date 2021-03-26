801e10579e57ed25cf34117a78756e6a2c5162d8
Duraid Maihoub <73091719+xDuraid@users.noreply.github.com>
2021-03-26 13:56:44 -0700

    Create Maximum Draws.cpp

---
+++ Mathematics/Maximum Draws.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int t = 0, n = 0;
+    cin >> t;
+    while(t--){
+        cin >> n;
+        cout << n + 1 << endl;
+    } 
+    return 0;
+}
